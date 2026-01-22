4cde162e69386a3401a90664ad19c043d4ce8499
MasterGamer153 <jorkobege@gmail.com>
2026-01-22 14:47:58 +0200

    angular reactive form implement

--- src/app/components/form/form.ts
+++ src/app/components/form/form.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms'
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './form.html',
   styleUrl: './form.scss',
 })
 export class Form {
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
+  onSubmit() {
+    console.log(this.loginForm.value)
+  }
 }
