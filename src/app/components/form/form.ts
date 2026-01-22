5021804001a5dcacf5cbefc88ae7a3796013bfb5
MasterGamer153 <jorkobege@gmail.com>
2026-01-22 16:47:26 +0200

    validation + error display

--- src/app/components/form/form.ts
+++ src/app/components/form/form.ts
@@ -1,20 +1,38 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms'
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms'
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './form.html',
   styleUrl: './form.scss',
 })
 export class Form {
   loginForm = new FormGroup({
-    email: new FormControl(''),
-    password: new FormControl(''),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+    ]),
   });
 
+  get email(){
+    return this.loginForm.get('email');
+  }
+  get password() {
+    return this.loginForm.get('password')
+  }
+
   onSubmit() {
+    if (this.loginForm.invalid){
+      return;
+    }
+
     console.log(this.loginForm.value)
   }
 }
