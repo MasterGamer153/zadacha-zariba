9b2de5ce6abdc021c9a8bf362ad8f4b4bd50d631
MasterGamer153 <jorkobege@gmail.com>
2026-01-22 13:51:34 +0200

    innate

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Form } from "./components/form/form";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Form],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('zadacha');
+}
