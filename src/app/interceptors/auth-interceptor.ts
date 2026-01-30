2d8a83cc3bd46e69734476541312fee9258de24a
MasterGamer153 <jorkobege@gmail.com>
2026-01-30 16:54:00 +0200

    Interceptor

---
+++ src/app/interceptors/auth-interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptorFn,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+ } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Auth } from '../services/auth'
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authService: Auth) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    
+    const token = this.authService.getToken();
+
+    if (!token) {
+      return next.handle(req);
+    }
+
+    const authReq = req.clone({
+      setHeaders: {
+        Authorization: 'Bearer ${token}'
+      }
+    })
+
+    return next.handle(authReq)
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './app/interceptors/auth-interceptor';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
+});
